d422fe78333046609c3a351d5be3fb57baeb6ac1
lam29121996 <lam29121996@gmail.com>
2023-01-16 12:14:13 -0500

    first commit

---
+++ js/hello.js
@@ -0,0 +1,6 @@
+function test() {
+    console.log('Hello World!');
+    var num1 = 10;
+    console.log(num1);
+}
+test();
---
+++ ts/hello.ts
@@ -0,0 +1,7 @@
+function test() {
+    console.log('Hello World!');
+    let num1:number = 10 ;
+    console.log(num1);
+}
+
+test();
